a909842849bb5ca907bfe3c963c11ab971b66ef8
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-11-15 20:39:35 +0300

    HW 14.11.2022

--- src/main/java/com/skypro/employeebookspring/record/EmployeeRequest.java
+++ src/main/java/com/skypro/employeebookspring/record/EmployeeRequest.java
@@ -1,6 +1,9 @@
 package com.skypro.employeebookspring.record;
 
+import static org.apache.commons.lang3.StringUtils.*;
+
 public class EmployeeRequest {
+    private static final String VALID_SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъэждлорпавфыячсмитьбюёЁЙЦУКЕНГШЩЗХЪЭЖДЛОРПАВЫФЯЧСМИТЬБЮ";
     private  String firstName;
     private  String lastName;
     private  int department;
@@ -10,16 +13,24 @@ public class EmployeeRequest {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String firstName) throws RuntimeException {
+        if (containsOnly(firstName, VALID_SYMBOLS)) {
+            this.firstName = capitalize(firstName);
+        } else {
+            throw new RuntimeException("Wrong first name!");
+        }
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName(String lastName) throws RuntimeException {
+        if (containsOnly(lastName, VALID_SYMBOLS)) {
+            this.lastName = capitalize(lastName);
+        } else {
+            throw new RuntimeException("Wrong last name!");
+        }
     }
 
     public int getDepartment() {
